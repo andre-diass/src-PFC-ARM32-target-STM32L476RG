b6fbeec35ac97ed3edf486e1c73206e7624d7e22
andre-diass <adsf@poli.br>
2024-08-14 14:46:27 -0300

    test gpio

--- Src/main.c
+++ Src/main.c
@@ -21,8 +21,21 @@
 
 int main(void)
 {
-	uint32_t *pClkCtrlReg =   (uint32_t*)0x40023830;
-	uint32_t *pPortDModeReg = (uint32_t*)0x40020C00;
-	uint32_t *pPortDOutReg =  (uint32_t*)0x40020C14;
-	for(;;);
+	uint32_t *pClkCtrlReg =   (uint32_t*)0x4002104c;
+	uint32_t *pPortBModeReg = (uint32_t*)0x48000400;
+	uint32_t *pPortBOutReg =  (uint32_t*)0x48000414;
+
+	//1. enable the clock for GPOIb peripheral in the AHB2 (SET the 2nd bit position)
+	*pClkCtrlReg |= 0x02;
+
+	//2. configure the mode of the IO pin as output
+	//a. clear the 24th and 25th bit positions (CLEAR)
+	*pPortBModeReg &= 0xFCFFFFFF;
+	//b. make 24th bit position as 1 (SET)
+	*pPortBModeReg |= 0x01000000;
+
+	//3.SET 12th bit of the output data register to make I/O pin-12 as HIGH
+	*pPortBOutReg |= 0x1000;
+
+    while(1);
 }
