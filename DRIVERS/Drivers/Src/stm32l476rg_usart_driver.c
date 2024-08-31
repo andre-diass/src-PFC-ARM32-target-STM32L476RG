90a9e11a8053449ce31c8566d9add9bfe007cee0
andre-diass <adsf@poli.br>
2024-08-31 20:32:21 -0300

    chore: implement USART peripheral clock control apis

---
+++ DRIVERS/Drivers/Src/stm32l476rg_usart_driver.c
@@ -0,0 +1,41 @@
+#include "stm32l4076rg_usart_driver.h"
+#include "stm32l476rg.h"
+
+
+void USART_PeripheralControl(USART_RegDef_t *pUSARTx, uint8_t Cmd)
+{
+	if(Cmd == ENABLE)
+	{
+		pUSARTx->CR1 |= (1 << 0); 
+	}else
+	{
+		pUSARTx->CR1 &= ~(1 << 0);
+	}
+
+}
+
+void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pUSARTx == USART1)
+		{
+			USART1_PCCK_EN();
+		}else if (pUSARTx == USART2)
+		{
+			USART2_PCCK_EN();
+		}else if (pUSARTx == USART3)
+		{
+			USART3_PCCK_EN();
+		}
+		else if (pUSARTx == UART4)
+		{
+			UART4_PCCK_EN();
+		}
+	}
+	else
+	{
+		//TODO
+	}
+
+}
